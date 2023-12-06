2382ed4e6f3fb78180e6291d320b99e27bc08de9
atbee <athibet.pi@gmail.com>
2023-12-06 15:49:12 +0700

    Create a home route

--- build/src/index.js
+++ build/src/index.js
@@ -5,9 +5,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
+const routes_1 = __importDefault(require("./routes"));
 class Server {
     constructor(app) {
         this.config(app);
+        new routes_1.default(app);
     }
     config(app) {
         const corsOptions = {
